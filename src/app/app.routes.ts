868c90ff91baf22557cd22f7a02f656d5deafee7
DABILGOU <dabilgou10@gmail.com>
2024-09-08 22:16:56 +0000

    Refactor app.routes.ts to remove unused imports and update component paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { WendComponent } from './wend/wend.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { EnginsComponent } from './components/engins/engins.component';
 import { GasoilComponent } from './components/gasoil/gasoil.component';
@@ -9,7 +8,6 @@ import { PersonnelComponent } from './components/personnel/personnel.component';
 import { PointageComponent } from './components/pointage/pointage.component';
 import { RapportPanneComponent } from './components/rapport-panne/rapport-panne.component';
 import { TravauxComponent } from './components/travaux/travaux.component';
-import { authGuard } from './auth.guard';
 import { Essai2Component } from './components/essai2/essai2.component';
 import { SousTraitanceComponent } from './components/sous-traitance/sous-traitance.component';
 import { ConstatsComponent } from './components/constats/constats.component';
@@ -17,7 +15,6 @@ import { AttachementsComponent } from './components/attachements/attachements.co
 import { PrestationComponent } from './components/prestation/prestation.component';
 import { DecomptesComponent } from './components/decomptes/decomptes.component';
 import { PointageTrvxEnginsComponent } from './components/pointage-trvx-engins/pointage-trvx-engins.component';
-import { TacheProjetStore } from './store/appstore';
 import { TableauBordComponent } from './components/tableau-bord/tableau-bord.component';
 
 export const routes: Routes = [{ 
