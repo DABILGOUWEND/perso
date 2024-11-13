bae48642f3f7425eac4e85df515a13080251089e
DABILGOU <dabilgou10@gmail.com>
2024-11-13 21:53:14 +0000

    hello

--- src/app/store/appstore.ts
+++ src/app/store/appstore.ts
@@ -5,6 +5,7 @@ import { Observable, concat, forkJoin, map, of, pipe, switchMap, tap } from "rxj
 import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { eleve_state } from "../models/modeles";
+import { get } from "http";
 
 const initial_eleve_state: eleve_state = {
     data_eleve: [],
@@ -17,10 +18,27 @@ const initial_eleve_state: eleve_state = {
 export const eleveçstore = signalStore(
     { providedIn: 'root' },
     withState(initial_eleve_state),
-    withComputed(()=>)
+    withComputed((store)=>({
+        taille: computed(()=>{
+            return store.data_eleve.length
+        }),
+    })),
     withMethods((store, snackbar = inject(MatSnackBar)) =>
     (
         {
+            getEleves: rxMethod((store, id: string) => {
+                return of(id).pipe(
+                    switchMap((id) => {
+                        return getDoc(doc(collectionData(collection(store.firestore, 'eleves')), id))
+                    }),
+                    map((doc) => {
+                        return doc.data() as eleve_state
+                    }),
+                    tap((data) => {
+                        store.dispatch(patchState(data))
+                    })
+                )
+            }),
             
 
         }
